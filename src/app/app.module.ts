a1dccd72490a57c534c2a1e7c70d9e4825de02ba
samoutinho <samuel.moutinho@keyks.com>
2021-10-15 17:43:18 -0300

    modularizando o componente error404  na pasta core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
-import { NavBarComponent } from './core/component/nav-bar/nav-bar.component';
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [AppComponent, Error404Component],
+  declarations: [AppComponent],
   imports: [
     CourseModule,
     BrowserModule,
@@ -22,10 +19,6 @@ import { CoreModule } from './core/core.module';
         path: '',
         redirectTo: 'courses',
         pathMatch: 'full',
-      },
-      {
-        path: '**',
-        component: Error404Component,
       },
     ]),
   ],
